1548ee79612d6a296ed84f9a5403721d556f53b1
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2022-11-22 15:23:22 +0100

    ajout des commentaires

--- Code/index.js
+++ Code/index.js
@@ -1,22 +1,22 @@
-// windows.localStorage.maboite = "je stock les données"
-// let mesDonnees = windows.localStorage.maboite
-
 const form = document.querySelector("form");
 
+//Function to use localStorage to save List
 const storage = () => {
-    window.localStorage.storageList = list.innerHTML;
+  window.localStorage.storageList = list.innerHTML;
+};
+//Function to get LocalStorage of the save List
+const getStorageList = () => {
+  if (window.localStorage.storageList) {
+    list.innerHTML = window.localStorage.storageList;
+  } else {
+    list.innerHTML = `<li>ᗢ Cliquez sur un Todo pour le supprimer</li>`;
   }
+};
 
-  const getStorageList = () => {
-    if (window.localStorage.storageList) {
-      list.innerHTML = window.localStorage.storageList;
-    } else {
-      list.innerHTML = `<li>ᗢ Cliquez sur un Todo pour le supprimer</li>`;
-    }
-  }
+//When the home page load, use Function getStorageList
+window.addEventListener("load", getStorageList);
 
-  window.addEventListener("load", getStorageList);
-
+//When the form is submitted, add value of the List
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -25,6 +25,7 @@ form.addEventListener("submit", (e) => {
   storage();
 });
 
+//When the The is clicked, delete selected value
 list.addEventListener("click", (e) => {
   e.target.remove();
   storage();
